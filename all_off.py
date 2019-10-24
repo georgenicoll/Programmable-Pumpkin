735505bfad49c326a2de02fee4e0984c7a1ea91a
George Nicoll <george.nicoll@gmail.com>
2019-10-24 22:05:56 +0100

    Add all_off.py

---
+++ all_off.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python
+
+#LED1 - 18 - 24
+#LED2 - 16 - 23
+#LED3 - 15 - 22
+#LED4 - 40 - 21
+#LED5 - 38 - 20
+#LED6 - 35 - 19
+#LED7 - 12 - 18
+#LED8 - 11 - 17
+#LED9 - 36 - 16
+#LED10 - 33 - 13
+#LED11 (GREEN) - 31 - 6
+#LED12 (GREEN) - 32 - 12
+
+from pumpkinpi import PumpkinPi
+from time import sleep
+
+pumpkin = PumpkinPi()
+
+pumpkin.off()
+pumpkin.close()
